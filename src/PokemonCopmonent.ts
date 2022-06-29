5fc4d0b5ac407bb82076471066c01c8381a839a1
shakedgo <shakedgo@gmail.com>
2022-06-30 00:14:17 +0300

    Added pokemon styles and catch (#2)
    * Added PokemonComponent - renders the name and the photo of the pokemon
    
    * Added pokemon styles and catch

--- src/PokemonCopmonent.ts
+++ src/PokemonCopmonent.ts
@@ -8,10 +8,15 @@ export class PokemonCopmonent {
 		this.parent = parent;
 		this.data = data;
 	}
-	render() {
-		return (this.parent!.innerHTML += `<div class="pokemon" id="pokemon-${this.data.sprites.front_default}">
+  
+  render() {
+    return (this.parent!.innerHTML += `<div class="pokemon-comp" id="pokemon-${this.data.sprites.front_default}">
   <img class="pokemon-img" src="${this.data.sprites.front_default}">
     <div class="pokemon-name">${this.data.name}</div>
+    <div class="pokemon-functions">
+    <img class="pokeball-img" onclick="pokemon.module.catch()" src="https://images.cults3d.com/gNmCucguF_950khaXUHO3dVUZLM=/516x516/https://files.cults3d.com/uploaders/19933232/illustration-file/58d154fb-ebc1-4f8b-bd76-a53f7c5c1ef5/2022_01_17_00_12_48_Pok%C3%A9ball_normal.pdf_et_1_page_suppl%C3%A9mentaire_Personnel_Microsoft_Edge.png" width="40px"/></a>
+    <a class="pokemon-abilities" href=''>Abilities</a>
+    </div>
   </div>`);
-	}
+  }
 }
